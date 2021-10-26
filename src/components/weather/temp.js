932d3055c9636c431b085c653b99deeb0586ad4e
Shubham Singh <shubhamsingh333333@gmail.com>
2021-10-26 12:28:54 +0530

    first commit

---
+++ src/components/weather/temp.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import Weathercard from "./weathercard";
+import "./style.css";
+
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("pune");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=92acb39d5aa20091aaf88b7687e66fbd`;
+
+      let res = await fetch(url);
+      let data = await res.json();  
+      console.log(data)
+
+      const { temp, humidity, pressure } = data.main;
+      console.log(data.main)
+       const { main: weathermood } = data.weather[0]; /*name alia */
+      const { name } = data;  /*city name direct */
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search the city..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* our temp card  */}
+      <Weathercard {...tempInfo} />  
+        {/* /* spread operator by default as destructuring it  */ }
+    </>
+  );
+};
+
+export default Temp;
